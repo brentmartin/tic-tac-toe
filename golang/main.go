c3ff05b4c2d5029fa554561b7399c006fbe8d214
Brent Martin <bmart35@gmail.com>
2016-08-25 23:59:38 -0500

    made a visual tic-tac-toe board with println()

--- golang/main.go
+++ golang/main.go
@@ -10,6 +10,18 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		os.Stdout.WriteString("\x1b[3;J\x1b[H\x1b[2J")
+		println("    Tic-tac-toe    ")
+		println("   Golang Edition  ")
+		println("╔════════╦════════╗")
+		println("║ X =", x, " ║ O =", o, " ║")
+		println("╚════════╩════════╝")
+		println("   ┍━━━━━━━━━━━┑   ")
+		println("   ⎟ 1 ║ 2 ║ 3 ⎟   ")
+		println("   ⎟═══╬═══╬═══⎟   ")
+		println("   ⎟ 4 ║ 5 ║ 6 ⎟   ")
+		println("   ⎟═══╬═══╬═══⎟   ")
+		println("   ⎟ 7 ║ 8 ║ 9 ⎟   ")
+		println("   ┕━━━━━━━━━━━┙   ")
 		fmt.Println(scanner.Text())
 	}
 }
