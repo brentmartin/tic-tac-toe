dea4d195810bee91954c2ada693df06ceda5aba9
Brent Martin <bmart35@gmail.com>
2016-08-26 23:59:35 -0500

    included an option for input

--- golang/main.go
+++ golang/main.go
@@ -29,6 +29,14 @@ func main() {
   boardAvailableMoves := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	playerOneMoves := []int
   playerTwoMoves := []int
+
+  var option string
+
+  fmt.Println("1) Play against Computer")
+  fmt.Println("2) Play against Player")
+  fmt.Print("Please choose an option: ")
+
+  fmt.Scanln(&option)
 func generateLogic(playerOneMoves []int, playerTwoMoves []int, boardAvailableMoves []int) {
 
 }
