e7b6c78dab72e3a99a367a0cad0dd2dab8c9a9d6
Brent Martin <bmart35@gmail.com>
2016-08-29 23:43:36 -0500

    refactored main function

--- golang/main.go
+++ golang/main.go
@@ -6,37 +6,34 @@ import (
 )
 
 func main() {
-
-  var option string
-
-  fmt.Println("1) Play against Computer")
-  fmt.Println("2) Play against Player")
-  fmt.Print("Please choose an option: ")
-
-  fmt.Scanln(&option)
-
-  switch option {
-  case "1":
-    generateLogic(playerOneMoves, playerTwoMoves, boardAvailableMoves)
-    generateBoard(playerOneScore, playerTwoScore)
-  case "2":
-    generateLogic(playerOneMoves, playerTwoMoves, boardAvailableMoves)
-    generateBoard(playerOneScore, playerTwoScore)
-  default:
-    fmt.Println("Uknown option, no action taken")
-  }
 	playerOneScore := 1
 	playerTwoScore := 1
 	boardAvailableMoves := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	playerOneMoves := make([]int, 1, 10)
 	playerTwoMoves := make([]int, 1, 10)
+
+	var option string
+
+	fmt.Println("1) Play against Computer")
+	fmt.Println("2) Play against Player")
+	fmt.Print("Please choose an option: ")
+
+	fmt.Scanln(&option)
+
+	switch option {
+	case "1":
+		generateLogic(playerOneMoves, playerTwoMoves, boardAvailableMoves)
+		generateBoard(playerOneScore, playerTwoScore)
+	case "2":
+		generateBoard(playerOneScore, playerTwoScore)
+	default:
+		fmt.Println("Uknown option, no action taken")
+	}
 }
 
 func generateLogic(playerOneMoves []int, playerTwoMoves []int, boardAvailableMoves []int) {
 
 }
-
-
 
   println("    Tic-tac-toe    ")
   println("   Golang Edition  ")
