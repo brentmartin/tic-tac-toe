bc3b8c0d004b1bc75e3f8313ab7ff9cac5b87e6f
Brent Martin <bmart35@gmail.com>
2016-09-01 23:58:53 -0500

    included pOneScore pTwoScore in the generateBoard function

--- golang/main.go
+++ golang/main.go
@@ -23,7 +23,7 @@ func main() {
 	var choice string
 
 	for i := 1; i <= 10; i++ {
-		generateBoard(gameOn, sq, pOneMoves, header)
+		generateBoard(gameOn, sq, pOneMoves, header, pOneScore, pTwoScore)
 
 		if gameOn == false {
 			fmt.Println("C) Play against Computer")
@@ -63,7 +63,7 @@ func main() {
 	}
 }
 
-func generateBoard(gameOn bool, sq []string, pOneMoves []int, header string) {
+func generateBoard(gameOn bool, sq []string, pOneMoves []int, header string, pOneScore int, pTwoScore int) {
 	if gameOn == false {
 		pOneMoves = make([]int, 1, 10)
 		sq = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
