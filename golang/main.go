b3d65326d9fb5ef4824a1a681096cfbce1cef7b4
Brent Martin <bmart35@gmail.com>
2016-09-01 20:27:32 -0500

    included an if statement in new generateBoard that will reset variables if bool = true

--- golang/main.go
+++ golang/main.go
@@ -65,6 +65,11 @@ func main() {
 }
 
 func generateBoard(gameOn bool, sq []string, pOneMoves []int) {
+	if gameOn == false {
+		pOneMoves = make([]int, 1, 10)
+		sq = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+		gameOn = true
+	}
 
 }
 
